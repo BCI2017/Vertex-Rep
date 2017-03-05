0de078c4116154ace4943590164f9ad512419c33
Tareq <tareqiissam@yahoo.com>
2017-03-05 20:26:07 +0200

    Adding the whole project

---
+++ Billing System/Billing System/Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using StudentsCol.Models;
+using System.Web.Security;
+using Billing_System;
+
+namespace StudentsCol.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection form)
+        {
+            string username = form["username"].ToString();
+            string password = form["password"].ToString();
+               
+            using (VertexEntities myEntity = new VertexEntities())
+            {
+                var user = myEntity.Ver_Users.Where(a => a.Username.Equals(username) && a.Password.Equals(password)).FirstOrDefault();
+                if (user != null)
+                {
+                    //Create seession/ token for loged in user
+                    FormsAuthentication.SetAuthCookie(username, false);
+                    return RedirectToAction("Index", "Dashboard");
+                    //MessageBox.Show("Test");          
+                }
+                if(username== "" && password=="")
+                {
+                    TempData["Message"] = null;               
+                }
+                else
+                {
+                    TempData["Message"] = "Username or password is wrong";
+                }       
+            }         
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+    }
+}
---
+++ Billing System/Billing System/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Billing_System.Controllers
+{
+    public class UsersController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
